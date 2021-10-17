d0ef54d83629adf906caad2877f8452e15926ef6
Justin Cavalli <justinc@quantum-ea.com>
2021-10-17 13:52:57 -0700

    added a few comments

--- Main.java
+++ Main.java
@@ -7,16 +7,21 @@ import java.io.IOException;
 public class Main {
 
     public static class Vertex {
+        // vertex object which have an x and y coordinates
+        // of the float type
         public float x;
         public float y;
         public Vertex(float x, float y) {
             this.x = x;
             this.y = y;
         }
+
+        // calculates distance between vertices
         static Vertex minus(Vertex p, Vertex q) {
             return new Vertex(p.x-q.x, p.y-q.y);
         }
 
+        // calculate the dot product of two vertices
         static float dot(Vertex p, Vertex q) {
             return p.x*q.x + p.y*q.y;
         }
@@ -29,7 +34,7 @@ public class Main {
         public float w;
 
         public float interpolate(float a, float b, float c) {
-            return a * u + b * v + w * c;   // finished TODO
+            return a * u + b * v + w * c;
         }
 
     }
@@ -38,11 +43,14 @@ public class Main {
         public int y1 = 200;
         public int x0 = 250;
         public int x1 = 250;
-
+        
+        // set the vertices of the triangle in which
+        // barcyentric coordinates are relatively calculated from
         public Vertex v0 = new Vertex(x0, y0);
         public Vertex v1 = new Vertex(x0 - (y1-y0+1), y1);
         public Vertex v2 = new Vertex(x0 + (y1-y0+1), y1);
 
+        // secondary colors
         // yellow
         public int r0 = 255;
         public int g0 = 255;
@@ -68,13 +76,14 @@ public class Main {
 
         Barycentric CalcBarycentric(Vertex p)
         {
-            // Cite: https://gamedev.stackexchange.com/questions/23743/whats-the-most-efficient-way-to-find-barycentric-coordinates
+            // barycentric coordinates are calculated based on the positioning
+            // of the pixel in relation to the vertices
             Vertex v0 = Vertex.minus(this.v1, this.v0);
             Vertex v1 = Vertex.minus(this.v2, this.v0);
             Vertex v2 = Vertex.minus(p, this.v0);
 
 
-            float d00 = Vertex.dot(v0, v0); // finished TODO
+            float d00 = Vertex.dot(v0, v0);
             float d01 = Vertex.dot(v0, v1);
             float d11 = Vertex.dot(v1, v1);
             float d20 = Vertex.dot(v2, v0);
@@ -98,13 +107,14 @@ public class Main {
 
         SimpleTriangle t = new SimpleTriangle();
 
+        // loop through every pixel in the triangle
         for(int y=t.y0; y<=t.y1; ++y) {
             for(int x=t.x0; x<=t.x1; ++x) {
                 Vertex p = new Vertex(x,y);
                 Barycentric bary = t.CalcBarycentric(p);
-
+                // linear interpolation calculation
                 int r = (int)(bary.interpolate(t.r0, t.r1, t.r2));
-                int g = (int)(bary.interpolate(t.g0, t.g1, t.g2)); // finished TODO
+                int g = (int)(bary.interpolate(t.g0, t.g1, t.g2));
                 int b = (int)(bary.interpolate(t.b0, t.b1, t.b2));
 
                 int color = b | (g<<8) | (r<<16);
